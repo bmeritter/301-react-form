3b0141e37e1f533c6234508f9e199bde268b21bb
bmeritter <bme_ritter@foxmail.com>
2017-02-08 14:55:50 +0800

    add .babelrc

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import {render} from 'react-dom';
 import '../style/index.less';
 
-ReactDom.render(
-    <h1>Hello,World!</h1>,
-    document.getElementById('app')
+render(
+  <h1>Hello,World!</h1>,
+  document.getElementById('app')
 );
