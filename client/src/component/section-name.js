cb5c521a659228145d6fa20ff3e883e3b303ce5a
bmeritter <bme_ritter@foxmail.com>
2017-02-13 00:41:12 +0800

    delete comfirm

--- client/src/component/section-name.js
+++ client/src/component/section-name.js
@@ -1,21 +1,60 @@
 import React, {Component} from 'react';
+import {Modal, Button} from 'react-bootstrap';
 
 export default class SectionName extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalState: false
+    };
+  }
+
+  showModal() {
+    this.setState({
+      modalState: true
+    });
+  }
+
+  hideModal() {
+    this.setState({
+      modalState: false
+    });
+  }
+
   deleteSection() {
+    this.hideModal();
+
     this.props.deleteSection({sectionId: this.props.sectionId});
   }
 
 
   render() {
     return (
-      <div className="section-name">
-        <span className="col-sm-1 ">{this.props.title}</span>
-        <i className="fa fa-edit"></i>
+      <div>
+        <div className="section-name">
+          <span className="col-sm-1 ">{this.props.title}</span>
+          <i className="fa fa-edit"></i>
 
-        <div className="delete">
-          <i className="fa fa-trash" onClick={this.deleteSection.bind(this)}></i>
+          <div className="delete">
+            <i className="fa fa-trash" onClick={this.showModal.bind(this)}></i>
+          </div>
         </div>
 
+        <Modal show={this.state.modalState}>
+          <Modal.Header>
+            <Modal.Title>删除提示</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body>
+            确定要删除这个 Section？
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button onClick={this.hideModal.bind(this)}>取消</Button>
+            <Button bsStyle="primary" onClick={this.deleteSection.bind(this)}>确定</Button>
+          </Modal.Footer>
+
+        </Modal>
       </div>
     );
   }
